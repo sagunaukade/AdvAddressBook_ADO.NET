e5fc010b6902f803ebee2bb5460b73016c3529cc
sagunaukade <sagunaukade96@gmail.com>
2022-03-06 00:48:06 +0530

    [Saguna] Welcome Mesage

---
+++ AdvAddressBook_ADO.NET/AdvAddressBook_ADO.NET/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvAddressBook_ADO.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Adv Address Book using ADO.NET");
+            Console.ReadLine();
+        }
+    }
+}
